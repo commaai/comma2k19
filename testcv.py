fb7165fe76ec53f0fb79ac3ef5090e27f9b9eb91
Yajiri <jenspeh@student.chalmers.se>
2024-04-04 16:27:59 +0200

    adding a dense optical flow to the testcv.py file, it now prints the magnitude and angle of movements on the screen

--- testcv.py
+++ testcv.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def draw_grid(frame, rows, cols):
     height, width, = frame.shape[:2]
@@ -16,9 +17,13 @@ def draw_grid(frame, rows, cols):
         cv2.line(frame, (x, 0), (x, height), (0, 255, 0), 1)
 
 #Open MP4
-video_capture = cv2.VideoCapture('../comma2k/Chunk_1/b0c9d2329ad1606b|2018-07-27--06-03-57/3/video.mp4')
+video_capture = cv2.VideoCapture('../comma2k/Chunk_1/b0c9d2329ad1606b|2018-07-27--06-03-57/3/video.hevc')
 rows = 10
 cols = 10
+ret, first_frame = video_capture.read()
+mask = np.zeros_like(first_frame)
+prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+mask[..., 1] = 255
 
 #Loop frames
 while True:
@@ -26,15 +31,29 @@ while True:
         print("error")
     #Read frames
     ret, frame = video_capture.read()
-
-    #Check if frame was read
-    if not ret:
-        break
     draw_grid(frame, rows,cols)
-    #Display frame
-    cv2.imshow('Frame with grid', frame)
-    #Check for key
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    #show input frame, add if wanted 
+    cv2.imshow("input", frame)
+    #convert to gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #calc dense optical flow
+    flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    #compute magnitude
+    magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    #print magnitude and angle
+    print("Magnitude:", magnitude)
+    print("Angle:", angle)
+    #set image hue
+    mask[..., 0] = angle * 180 / np.pi /2
+    #set image value
+    mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+    #convert hsv to rgb
+    rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
+
+    #update previous frame
+    prev_gray = gray
+    #Frames are read by intervals of 1 millisecond. The programs breaks out of the while loop when the user presses the "q" key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 #realese the video capture
 cv2.getBuildInformation()
